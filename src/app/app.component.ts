f4b42703ded4f9954b16c34a18e2ef138c94b303
molegario <molegario@x2omedia.com>
2025-04-05 15:48:38 -0400

    reimplemented appcomponent with signal tracking of state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from '../dummy-users';
@@ -16,14 +16,22 @@ import { TaskslistHeaderComponent } from './taskslist/tasksheader.component';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  users = DUMMY_USERS;
-  selected = DUMMY_USERS[0].id;
+  // users = DUMMY_USERS;
+  // selected = DUMMY_USERS[0].id;
 
-  get selectedUser() {
-    return this.users.find((user) => user.id === this.selected);
+  // get selectedUser() {
+  //   return this.users.find((user) => user.id === this.selected);
+  // }
+
+  // onSelected(selectedId:  string) {
+  //   this.selected = selectedId;
+  // }
+  users = signal(DUMMY_USERS);
+  selected = signal(DUMMY_USERS[0].id);
+  selectedUser = computed(() => this.users().find((user) => user.id === this.selected()));
+  onSelected(selectedId: string) {
+    this.selected.set(selectedId);
   }
 
-  onSelected(selectedId:  string) {
-    this.selected = selectedId;
-  }
+
 }
